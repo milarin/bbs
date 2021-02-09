7f480450c4cddf67b7dc5af7699ce792e39d9208
milan <acmilanpooh@gmail.com>
2021-02-09 12:32:32 +0900

    first commit

---
+++ bbs/resources/views/index.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset='utf-8'>
+        <title>hello bbs</title>
+        <style>body {padding: 10px;}</style>
+    </head>
+    <body>
+        <h1>hello bbs</h1>
+        <p>{{ $message }}</p>
+        @foreach ($articles as $article)
+            <p>
+              <a href="{{ route('article.show', ['id' => $article->id]) }}">{{ $article->content }}</a>
+            </p>
+        @endforeach
+    </body>
+</html>
