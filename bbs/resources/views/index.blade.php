1fb3f1a98533ace7ec0c65ee607ca0bd6eb62a38
milan <acmilanpooh@gmail.com>
2021-02-10 14:15:15 +0900

    layout.blade.php 追加　その周り変更

--- bbs/resources/views/index.blade.php
+++ bbs/resources/views/index.blade.php
@@ -1,23 +1,18 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset='utf-8'>
-        <title>hello bbs</title>
-        <style>body {padding: 10px;}</style>
-    </head>
-    <body>
-        <h1>hello bbs</h1>
-        <p>{{ $message }}</p>
-        @foreach ($articles as $article)
-            <p>
+@extends('layout')
+
+@section('content')
+    <h1>Hello bbs</h1>
+    <p>{{ $message }}</p>
+    @foreach ($articles as $article)
+        <p>
             <a href='{{ route("article.show", ["id" =>  $article->id]) }}'>
-                    {{ $article->content }},
-                    {{ $article->user_name }}
-                </a>
-            </p>
-        @endforeach
-        <div>
-            <a href="{{ route('article.new') }}">新規投稿</a>
-        </div>
-    </body>
-</html>
+                {{ $article->content }},
+                {{ $article->user_name }}
+            </a>
+        </p>
+    @endforeach
+    
+    <div>
+        <a href={{ route('article.new') }}>新規投稿</a>
+    </div>
+@endsection
