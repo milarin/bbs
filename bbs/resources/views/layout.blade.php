f9279e68366410ad55e9ba5bfa358497d0cf8ae7
milan <acmilanpooh@gmail.com>
2021-02-10 23:46:02 +0900

    nav style-sheet 追加

--- bbs/resources/views/layout.blade.php
+++ bbs/resources/views/layout.blade.php
@@ -3,9 +3,12 @@
     <head>
         <meta charset='utf-8'>
         <title>paiza bbs</title>
-        <style>body {padding: 10px;}</style>
+        @include('style-sheet')
     </head>
     <body>
+      @include('nav')
+      <div class='container'>
         @yield('content')
+      </div>
     </body>
 </html>
